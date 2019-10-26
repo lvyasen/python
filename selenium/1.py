53f742a811b9a5f8e1a99fad53ad54cc1a1ffd3e
walker <18530893662@163.com>
2019-10-26 18:43:35 +0800

    微博自动化登录

--- selenium/1.py
+++ selenium/1.py
@@ -1,22 +1,51 @@
 from selenium import webdriver
 import time
 
-#微博登录
+# 微博登录
 
 browser = webdriver.Chrome()
 
-def login(username,pwd):
-    browser.get("https://passport.weibo.cn/signin/login")
+
+# 加好友
+def add_friends(uid):
+    browser.get("https://m.weibo.com/u/" + str(uid))
     browser.implicitly_wait(5)
+    follow_btn = browser.find_element_by_xpath('//div[@class="m-add-box m-followBtn"]')
+    follow_btn.click()
     time.sleep(1)
-    #填写登录账号密码
+    # 选择分组
+    group_btn = browser.find_element_by_xpath(
+        "/html/body/div[@id='app']/div[1]/div[2]/div[@class='m-pop m-pop-lt']/section/ul[@class='gp-list']/li[7]/label[@class='m-checkbox']")
+    group_btn.click()
+    time.sleep(1)
+    sure_btn = browser.find_element_by_xpath(
+        "/html/body/div[@id='app']/div[1]/div[2]/div[@class='m-pop m-pop-lt']/footer[@class='m-btm-btns m-box']/div[@class='m-box-col'][2]/a[@class='m-btn m-btn-white m-btn-text-orange']")
+    sure_btn.click()
+#发微博
+def post_content(content):
+    browser.get('https://m.weibo.cn/compose/')
+    browser.implicitly_wait(2)
+
+    content_textarea = browser.find_element_by_xpath("//span[@class='m-wz-def']/textarea").send_keys(content)
+    time.sleep(1)
+    post_submit = browser.find_element_by_xpath("//a[@class='m-send-btn']").click()
+
+
+def login(username, pwd):
+    browser.get("https://passport.weibo.cn/signin/login")
+    browser.implicitly_wait(2)
+    time.sleep(1)
+    # 填写登录账号密码
     browser.find_element_by_id("loginName").send_keys(username)
     browser.find_element_by_id("loginPassword").send_keys(pwd)
     time.sleep(1)
-    #点击登录
+    # 点击登录
     browser.find_element_by_id("loginAction").click()
-    time.sleep()
+    time.sleep(1)
+    content = "你们看“肖战小号”这个事了吗？有黑粉用时1个月，养了3个微博账号，通过伪造行程定位、盗图、伪造情侣头像等手段，冒充是肖战的小号，冒充肖战的工作人员的账号，故意误导吃瓜群众认为他有男友。结果被另一波黑粉撞破，提前暴露了。"
+    post_content(content)
+    # add_friends(1618051664)
 
 username = '18530893662'
 pwd = 'lvyasen123.'
-login(username,pwd)
+login(username, pwd)
